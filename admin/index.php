0cc7bf55af788b0fc2acfb3737f2faed9aa6725b
graumanix <graumanix@gmail.com>
2020-11-19 20:56:38 +0100

    css register index div blanco

--- admin/index.php
+++ admin/index.php
@@ -77,7 +77,7 @@
                         <!-- Nested Row within Card Body -->
                         <div class="row justify-content-center">
                             <!--<div class="col-lg-6 d-none d-lg-block bg-login-image"></div>-->
-                            <div class="col-lg-6.5">
+                            <div class="col-lg-9">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Bienvenido</h1>
